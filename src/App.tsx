495766029cbf7c362dd20a3147a79f1885b6f889
Marcelo Haubrih <marcelohaubrih@gmail.com>
2020-07-01 13:34:03 -0300

    Add: Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import GlobalStyled from './styles/global';
 
-const App: React.FC = () => <h1>GoBarber</h1>;
+const App: React.FC = () => (
+  <>
+    <h1>GoBarber</h1>
+    <GlobalStyled />
+  </>
+);
 
 export default App;
